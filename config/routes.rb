bb1ff0c634ed6f09b61702785a143ad5a14d4d7b
AlexKVal <alexkval@gmail.com>
2017-04-13 20:30:22 +0300

    set new root route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
 
   resources :microposts
   resources :users
-  root 'users#index'
+
+  root 'static_pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

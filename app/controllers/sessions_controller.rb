268ce0c87b2bc7d0ccb5fa912b024fd5f547b8c4
AlexKVal <alexkval@gmail.com>
2017-05-06 19:32:25 +0300

    login with invalid information

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user&.authenticate(params[:session][:password])
+      render text: 'OK'
+    else
+      flash.now[:danger] = "Incorrect email/password"
+      render 'new'
+    end
+  end
+
+  def destroy
+    #code
+  end
 end

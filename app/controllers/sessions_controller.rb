688e0098599ae7c6e87a0af1db606be89cf93b57
AlexKVal <alexkval@gmail.com>
2017-05-06 20:31:07 +0300

    add #current_user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
-      render text: 'OK'
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = "Incorrect email/password"
       render 'new'

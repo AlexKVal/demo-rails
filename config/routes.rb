35b9610cdefb9bfa514997e393d38a4ff4368589
AlexKVal <alexkval@gmail.com>
2017-05-12 16:01:24 +0300

    add "update profile"

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
 
   get 'signup', to: 'users#new'
   post 'signup', to: 'users#create'
-  patch 'signup', to: 'users#update'
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'

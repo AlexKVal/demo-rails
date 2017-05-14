1d1d1c3ffed869be2731f75a769eac6261c2d852
AlexKVal <alexkval@gmail.com>
2017-05-14 19:43:53 +0300

    add password reset link, form and fields

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,5 @@ Rails.application.routes.draw do
   resources :users
 
   resources :account_activations, only: [:edit]
-
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end

898c8f890a237dea2d62b71365a9bc86b970d4b2
AlexKVal <alexkval@gmail.com>
2017-05-16 00:43:34 +0300

    microposts validate user_id presence

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,17 +1,13 @@
 require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
-  def setup
-    @from_email = ENV['MJ_ADDRESS'] = 'sample@email.com'
-  end
-
   test "account_activation" do
     user = users(:michael)
     user.activation_token = User.new_token
     mail = UserMailer.account_activation(user)
     assert_equal "Account activation", mail.subject
     assert_equal [user.email], mail.to
-    assert_equal [@from_email], mail.from
+    assert_equal [ENV['MJ_ADDRESS']], mail.from
     assert_match user.name,              mail.body.encoded
     assert_match user.activation_token,  mail.body.encoded
     assert_match CGI.escape(user.email), mail.body.encoded
@@ -23,7 +19,7 @@ class UserMailerTest < ActionMailer::TestCase
     mail = UserMailer.password_reset(user)
     assert_equal "Password reset", mail.subject
     assert_equal [user.email], mail.to
-    assert_equal [@from_email], mail.from
+    assert_equal [ENV['MJ_ADDRESS']], mail.from
     assert_match user.reset_token,       mail.body.encoded
     assert_match CGI.escape(user.email), mail.body.encoded
   end

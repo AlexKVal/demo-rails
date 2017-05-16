03b96363081012836f1c924858bde11e9c542038
AlexKVal <alexkval@gmail.com>
2017-05-17 01:22:46 +0300

    implement posting new microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -3,9 +3,21 @@ class MicropostsController < ApplicationController
 
   # POST /microposts
   def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      redirect_to root_path, success: "Micropost published"
+    else
+      render 'static_pages/home'
+    end
   end
 
   # DELETE /microposts/1
   def destroy
   end
+
+  private
+
+    def micropost_params
+      params.require(:micropost).permit(:content)
+    end
 end

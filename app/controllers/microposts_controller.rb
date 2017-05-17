0673c1816ed8d52af0e4fd176d7e88afaf833c6f
AlexKVal <alexkval@gmail.com>
2017-05-17 19:54:14 +0300

    fix micropost's error state

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,12 +1,24 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
 
+  # a quick and dirty hack for showing paginated microposts
+  # TODO: DRY it with StaticPagesController#home
+  def index
+    if logged_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.page(params[:page])
+    end
+    render 'static_pages/home'
+  end
+
   # POST /microposts
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
       redirect_to root_path, success: "Micropost published"
     else
+      # @feed_items = []
+      @feed_items = current_user.feed.page(params[:page])
       render 'static_pages/home'
     end
   end

7bf9265ab5dc60cb6e17b959698a8b20b6352237
AlexKVal <alexkval@gmail.com>
2017-05-20 22:10:32 +0300

    add follow/unfollow button

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -10,7 +10,9 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     assert_template 'users/show'
     assert_select '.panel-title img.gravatar'
     assert_select '.panel-title', text: @user.name
-    assert_select '.badge', @user.microposts.count.to_s
+    assert_select '#following', "2" # text: @user.following.count.to_s
+    assert_select '#followers', "1" # text: @user.followers.count.to_s
+    assert_select '.panel-title .badge', text: @user.microposts.count.to_s
     assert_select '.pagination', count: 1
     @user.microposts.page(1).each do |micropost|
       assert_match micropost.content, response.body

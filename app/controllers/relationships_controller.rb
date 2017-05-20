676c6af4e6c844c519ea52d3e0a30b5b2c8101f1
AlexKVal <alexkval@gmail.com>
2017-05-21 02:06:26 +0300

    implement protection against double-clicks from JS disabled browsers

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,13 +3,21 @@ class RelationshipsController < ApplicationController
 
   def create
     user = User.find(params[:followed_id])
-    current_user.follow(user)
-    redirect_to user
+    current_user.follow(user) unless current_user.following?(user)
+    respond_to do |format|
+      format.html { redirect_to user }
+      format.js
+    end
   end
 
   def destroy
     user = Relationship.find(params[:id]).followed
-    current_user.unfollow(user)
-    redirect_to user
+    current_user.unfollow(user) if user
+    respond_to do |format|
+      format.html { redirect_to user }
+      format.js
+    end
+  rescue ActiveRecord::RecordNotFound
+    redirect_back(fallback_location: root_path)
   end
 end

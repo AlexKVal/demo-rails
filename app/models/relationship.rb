934ae93db90c0d9a097dbce8111eca919ec4ba7b
AlexKVal <alexkval@gmail.com>
2017-05-20 17:28:55 +0300

    a user has many 'following'

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,8 @@
 class Relationship < ApplicationRecord
   belongs_to :follower, class_name: 'User'
   belongs_to :followed, class_name: 'User'
+
+  # Rails 5 makes belongs_to association required by default
+  # validates :follower_id, presence: true
+  # validates :followed_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name: 'Relationship',
                                   foreign_key: 'follower_id',
                                   dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
 
   before_create :create_activation_digest
   before_save { self.email.downcase! }

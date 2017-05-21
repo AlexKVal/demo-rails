3fa38f113d1e181b4da8551765fe6b72daf1ec21
AlexKVal <alexkval@gmail.com>
2017-05-22 02:12:21 +0300

    make it explicit that follower and followed are users

--- db/migrate/20170519212221_create_relationships.rb
+++ db/migrate/20170519212221_create_relationships.rb
@@ -1,8 +1,8 @@
 class CreateRelationships < ActiveRecord::Migration[5.0]
   def change
     create_table :relationships do |t|
-      t.references :follower, foreign_key: true, null: false
-      t.references :followed, foreign_key: true, null: false
+      t.references :follower, references: :users, foreign_key: true, null: false
+      t.references :followed, references: :users, foreign_key: true, null: false
 
       t.timestamps
     end

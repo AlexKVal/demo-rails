f022c26c44a164121465e1cdd1b2e23c452d2a00
AlexKVal <alexkval@gmail.com>
2017-05-22 15:01:33 +0300

    fix file uploading javascript

--- app/assets/javascripts/file_input.js
+++ app/assets/javascripts/file_input.js
@@ -1,15 +1,11 @@
-$(function () {
+$(document).on('change', '#micropost_picture', function() {
+  var label = $(this).val().split('\\').pop()
+  $('#file_name').val(label)
 
-  $('#micropost_picture').on('change', function() {
-    var label = $(this).val().split('\\').pop()
-    $('#file_name').val(label)
-
-    var fileSizeInMB = this.files[0].size / 1024 / 1024
-    if (fileSizeInMB > 5) {
-      alert('Maximum file size is 5MB. Please choose a smaller file.')
-      $(this).val('')
-      $('#file_name').val('')
-    }
-  })
-
+  var fileSizeInMB = this.files[0].size / 1024 / 1024
+  if (fileSizeInMB > 5) {
+    alert('Maximum file size is 5MB. Please choose a smaller file.')
+    $(this).val('')
+    $('#file_name').val('')
+  }
 })

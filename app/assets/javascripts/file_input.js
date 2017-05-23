0950209f8d1996049386aa55b7a8ecb3cd8362c1
AlexKVal <alexkval@gmail.com>
2017-05-23 23:44:39 +0300

    add an 'unselect-picture' button

--- app/assets/javascripts/file_input.js
+++ app/assets/javascripts/file_input.js
@@ -1,11 +1,25 @@
 $(document).on('change', '#micropost_picture', function() {
-  var label = $(this).val().split('\\').pop()
-  $('#file_name').val(label)
-
+  // file size validation
   var fileSizeInMB = this.files[0].size / 1024 / 1024
   if (fileSizeInMB > 5) {
     alert('Maximum file size is 5MB. Please choose a smaller file.')
     $(this).val('')
     $('#file_name').val('')
   }
+
+  // show a file name in the readonly input field
+  var label = $(this).val().split('\\').pop()
+  $('#file_name').val(label)
+
+  // show unselect-picture button
+  $('.unselect-picture').removeClass('hidden')
+})
+
+$(document).on('click', '.unselect-picture button', function() {
+  $('.unselect-picture').addClass('hidden')
+  // clear the selected picture
+  $('#micropost_picture').val('')
+  $('#file_name').val('')
+  // prevent default and propagation
+  return false
 })

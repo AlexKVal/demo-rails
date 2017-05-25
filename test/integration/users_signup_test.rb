4cc09a8088e48e1ae17e2e43fd1e25888f2d33e0
AlexKVal <alexkval@gmail.com>
2017-05-26 00:57:29 +0300

    implement sign-up showing errors with ajax

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -19,20 +19,35 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select '#error_explanation .alert', 'The form contains 2 errors.'
   end
 
+  test "invalid signup information with Ajax" do
+    get root_path
+    assert_template 'shared/_signup_form'
+    assert_no_difference 'User.count' do
+      post signup_path, xhr: true, params: { user: {
+        name: "John Dow",
+        email: "invalid email",
+        password: ""
+      } }
+    end
+    assert_template 'users/create'
+    assert_equal "text/javascript", response.content_type
+    assert_match "Email is invalid", response.body
+    # todo: to prevent such blemish, use Capybara for testing ajax
+    assert_match "Password can\\'t be blank", CGI.unescapeHTML(response.body)
+  end
+
   test "valid signup information with Ajax" do
     get root_path
     assert_template 'shared/_signup_form'
     assert_difference 'User.count', +1 do
-      post signup_path, params: { user: {
+      post signup_path, xhr: true, params: { user: {
         name: "John Dow",
         email: "john@example.com",
         password: "foobar"
       } }
     end
-    assert_redirected_to root_path
-    follow_redirect!
     assert_not flash.empty?
-
+    assert_equal "text/javascript", response.content_type
     assert_equal 1, ActionMailer::Base.deliveries.size
   end
 

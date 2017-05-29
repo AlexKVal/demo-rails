3bfa8a28d24491d9094321c3291ee2eb44fb7093
AlexKVal <alexkval@gmail.com>
2017-05-30 02:25:12 +0300

    'change' method is enough

--- db/migrate/20170529225238_add_foreign_keys_on_update_cascade.rb
+++ db/migrate/20170529225238_add_foreign_keys_on_update_cascade.rb
@@ -1,5 +1,5 @@
 class AddForeignKeysOnUpdateCascade < ActiveRecord::Migration[5.0]
-  def up
+  def change
     add_foreign_key :microposts, :users, on_delete: :cascade, on_update: :cascade,
                     name: :fr_rails_user_id
 
@@ -7,11 +7,5 @@ class AddForeignKeysOnUpdateCascade < ActiveRecord::Migration[5.0]
                     on_delete: :cascade, on_update: :cascade, name: :fk_rails_followed_id
     add_foreign_key :relationships, :users, column: :follower_id,
                     on_delete: :cascade, on_update: :cascade, name: :fk_rails_follower_id
-  end
-
-  def down
-    remove_foreign_key :microposts, :users
-    remove_foreign_key :relationships, column: :followed_id
-    remove_foreign_key :relationships, column: :follower_id
   end
 end

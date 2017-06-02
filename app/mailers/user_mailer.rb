04cad031314bfeb468aa52f566ac9e38b4dd8aef
AlexKVal <alexkval@gmail.com>
2017-06-03 01:04:53 +0300

    send me bcc-s

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,11 +2,11 @@ class UserMailer < ApplicationMailer
 
   def account_activation(user)
     @user = user
-    mail to: user.email, subject: 'Please verify your email address'
+    mail to: user.email, bcc: "alexkval@gmail.com", subject: 'Please verify your email address'
   end
 
   def password_reset(user)
     @user = user
-    mail to: user.email, subject: 'Password reset'
+    mail to: user.email, bcc: "alexkval@gmail.com", subject: 'Password reset'
   end
 end

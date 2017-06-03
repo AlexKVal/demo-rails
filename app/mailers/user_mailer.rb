d2714ce333cece25120c845403de4049fbb117f6
AlexKVal <alexkval@gmail.com>
2017-06-03 14:22:54 +0300

    style up account activation email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,29 @@
 class UserMailer < ApplicationMailer
+  def admin_email
+    'alexkval@gmail.com'
+  end
 
   def account_activation(user)
     @user = user
-    mail to: user.email, bcc: "alexkval@gmail.com", subject: 'Please verify your email address'
+    mail(
+      to: user.email,
+      bcc: admin_email,
+      subject: 'Please verify your email address'
+    ) do |format|
+      format.text
+      format.mjml
+    end
   end
 
   def password_reset(user)
     @user = user
-    mail to: user.email, bcc: "alexkval@gmail.com", subject: 'Password reset'
+    mail(
+      to: user.email,
+      bcc: admin_email,
+      subject: 'Password reset'
+    ) do |format|
+      format.text
+      format.html
+    end
   end
 end

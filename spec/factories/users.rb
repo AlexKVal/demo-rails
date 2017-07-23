24e7ee8ead1cda52d0e537f11c3aff3d5052c32b
AlexKVal <alexkval@gmail.com>
2017-07-23 19:19:28 +0300

    let user's fixture be activated_user by default

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,16 +1,15 @@
 FactoryGirl.define do
-  factory :user do
+  factory :user, aliases: [:activated_user] do
     name { Faker::Name.first_name }
     email { Faker::Internet.email }
 
     password "password1"
 
+    activated true
+    activated_at { 1.hour.ago }
+
     factory :not_activated_user do
       activated false
-    end
-
-    factory :activated_user do
-      after(:create, &:activate)
     end
   end
 end

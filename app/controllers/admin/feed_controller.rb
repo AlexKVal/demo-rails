89c44d45c0ce947b5a5ecbda0369bb2513602338
AlexKVal <alexkval@gmail.com>
2017-08-24 01:00:23 +0300

    Render initial data via transit template

--- app/controllers/admin/feed_controller.rb
+++ app/controllers/admin/feed_controller.rb
@@ -5,29 +5,9 @@ module Admin
 
     def index
       respond_to do |format|
-        format.html {
-          @init = render_to_string transit: init_data
-        }
+        format.html # initial data now render in the template: render "feed_init_data"
         # format.transit { render transit: full_data }
       end
-    end
-
-    private
-
-    def build_user(user)
-      { :id => user.id,
-        :name => user.name,
-        :email => user.email }
-    end
-
-    def build_micropost(post)
-      { :id => post.id,
-        :content => post.content,
-        :user => build_user(post.user)}
-    end
-
-    def init_data
-      { :microposts => microposts.map {|m| build_micropost(m)} }
     end
   end
 end
